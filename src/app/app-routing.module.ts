3024e5a6aaf06669e1a51a59f0c58a6c393451ee
Jesus163174 <163174@ids.upchiapas.edu.mx>
2019-07-25 19:45:13 -0500

    auth pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,9 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', loadChildren: './tabs/tabs.module#TabsPageModule' }
+  { path: '', loadChildren: './tabs/tabs.module#TabsPageModule' },
+  { path: 'login', loadChildren: './auth/user/pages/login/login/login.module#LoginPageModule' },
+  { path: 'register', loadChildren: './auth/user/pages/register/register/register.module#RegisterPageModule' }
 ];
 @NgModule({
   imports: [
